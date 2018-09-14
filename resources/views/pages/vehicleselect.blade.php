4321cf5e9d2b013d3cf0ce220b7591b3f995a8c8
lahirutech <rmllbrathnayaka@gmail.com>
2018-09-15 02:18:03 +0530

    Image gallery Out Look Changes

--- resources/views/pages/vehicleselect.blade.php
+++ resources/views/pages/vehicleselect.blade.php
@@ -8,7 +8,6 @@
 
         <div class="container">
           <div class="py-5 text-center">
-            <img class="d-block mx-auto mb-4" src="../../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
             <h2>Select your Vehicle</h2>
             {{-- image gallery --}}
             @include('inc.gallery')
