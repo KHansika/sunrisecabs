fd9543859ed3508c5b670d0ea6834378bdcde5fa
lahirutech <rmllbrathnayaka@gmail.com>
2018-09-15 23:16:42 +0530

    Adding Jquery links

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,12 +21,6 @@
 <body>
     @include('admin.adminnav')
     @yield('content')
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    <script src="{{asset('js/popper.min.js')}}"></script>
-    <!-- Icons -->
-    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-    <script>
-      feather.replace()
-    </script>
+    
 
 </body>
