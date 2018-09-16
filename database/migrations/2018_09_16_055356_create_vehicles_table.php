5d23fad5357042f167018ece32a60d343d57358a
lahirutech <rmllbrathnayaka@gmail.com>
2018-09-16 13:00:57 +0530

    Creating the vehicles table migration

---
+++ database/migrations/2018_09_16_055356_create_vehicles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('regno');
+            $table->string('plateno');
+            $table->string('type');
+            $table->float('price');
+            $table->string('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
