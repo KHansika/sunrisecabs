65f088a2543a0821369191d70a1787cf116191d6
Hirasha Pooliyadda <37568672+Hirasha@users.noreply.github.com>
2018-09-19 01:38:49 +0530

    Update services.blade.php

---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Sunrise Cabs</title>
+
+     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script type="text/javascript" src="{{asset('js/main.js')}}"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/mdb.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/business-frontpage.css')}}" rel="stylesheet">
+    <style>
+    .business-header {
+ background: url("../img/cover/carrental.jpg")no-repeat center center fixed;
+ height: 50vh;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+</style>
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark primary-color fixed-top">
+
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="#">Sunrise Cabs</a>
+
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse justify-content-center" id="basicExampleNav">
+
+        <!-- Links -->
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/">Home
+                    <span class="sr-only">(current)</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/cars">Cars</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="/services">Services</a>
+            </li>
+            <li class="nav-item">
+                    <a class="nav-link" href="/team">Team</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/about">About Us</a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="/contact">Contact</a>
+                </li>
+
+                <!-- Right navbar elements -->
+                
+                <!-- right nav bar elements -->
+                
+        </ul>
+        <!-- Links -->
+    </div>
+</nav>
+
+    <!-- Header with Background Image -->
+    <header class = "business-header">
+      
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+        <h2 class="mt-4"><b>Our Services</h2>
+      <div class="row" style="margin-top:10px;">
+        <div class="col-sm-4">
+          <img src="../img/cover/self.png" style="height:250px;" alt="">  
+        </div>      
+       <div class="col-sm-8">
+          <h4 class= "text-center"><b>Chauffeur Driven Vehicles</b></h4>
+          <p>Have a completely hassle free experience for car rental in Sri Lanka with our Chauffeur Driven cars which also includes Fuel and Insurance charges. This service aims at offering maximum convenience to our customers and you need not worry about licenses or pick-up and drop-off charges. Your chauffeur from Kings Rent a Car will ensure that you have a safe journey while you sit back and enjoy the ride. Chauffeur Driven Vehicles [Pricing includes charges for Driver, Fuel and Insurance</p>
+        </div>
+
+        <div class="row" style="margin-top:10px;">
+          <div class="col-sm-4">
+          <img src="../img/cover/wedding.png" style="height:250px;" alt="">  
+        </div>      
+       <div class="col-sm-8">
+          <h4 class= "text-center"><b>Wedding cars</b></h4>
+          <p>Do you want a dramatic arrival in a wedding car that leaves a lasting impression with a difference? Then Ameri Rent A Car Sri Lanka is your best choice for wedding car rentals. Whether it is a general vehicle, luxury car or a vintage model, we have got it all covered. Plus we ensure that there is always a backup in case of an unlikely emergency. We promise to exceed your expectations and guarantee the wedding day’s transport runs smoothly and in a punctual manner.</p>
+        </div>
+
+        <div class="row" style="margin-top:10px;">
+          <div class="col-sm-4">
+          <img src="../img/cover/fleetmgt.png" style="height:250px;" alt="">  
+        </div>      
+       <div class="col-sm-8">
+          <h4 class= "text-center"><b>Fleet Management</b></h4>
+          <p>Kings Rent A Car Colombo provides a professional full-time service, managing corporate vehicle fleets which in turn can save you time and money. Since we focus on the maintenance of vehicles, our customers can run their business in a cost-effective manner and also receive tax benefits.
+
+          We provide you with the highest quality services that address all your needs for fleet management at Kings Rent A Car, and we also assemble a customized program that is ideal for you and your business.</p>
+         </div>
+      
+      <!-- /.row -->
+
+    </div>
+  </div>
+</div>
+    <!-- /.container -->
+    
+
+    <!-- Bootstrap core JavaScript -->
+    
+
+  </body>
+
+</html>
