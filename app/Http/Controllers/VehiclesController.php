7a5f35e1f74e5fa69dfc0437572259037f273827
lahirutech <rmllbrathnayaka@gmail.com>
2018-09-18 18:02:32 +0530

    Adding Booking Model to vehicle controller

--- app/Http/Controllers/VehiclesController.php
+++ app/Http/Controllers/VehiclesController.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Vehicle;
 use Illuminate\Http\Request;
 
 class VehiclesController extends Controller
 {
     //
+    public function index(){
+        return view('admin.pages.vehiclefleet');
+    }
 }
