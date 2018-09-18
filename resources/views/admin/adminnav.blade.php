ceecf0b4b5cde4d2cb20bb98390c14bf03f623c3
lahirutech <rmllbrathnayaka@gmail.com>
2018-09-18 23:08:25 +0530

    Editing the navigation bar

--- resources/views/admin/adminnav.blade.php
+++ resources/views/admin/adminnav.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-dark fixed-top bg-primary flex-md-nowrap p-0 ">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Sunrise Cabs</a>
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/booking">Sunrise Cabs</a>
         <ul class="navbar-nav px-3">
           <li class="nav-item text-nowrap">
-            <a class="nav-link" href="#">Sign out</a>
+            <a class="nav-link" href="">Sign out</a>
           </li>
         </ul>
 </nav>
