fd9d0d9b300ee9247ff9344ec9f19f486f8f9903
lahirutech <rmllbrathnayaka@gmail.com>
2018-09-18 18:00:37 +0530

    Creting the controller for Admin Pages

---
+++ app/Http/Controllers/AdminpagesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Booking;
+
+class AdminpagesController extends Controller
+{
+    //
+    public function viewit ()
+    {
+        //view for the admin
+        $bookings= Booking::where('status',0)->get();
+        return view('admin.pages.neworders')->with('bookings',$bookings);
+    }
+}
