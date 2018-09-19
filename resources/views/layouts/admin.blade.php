cdf918551b8574b4c256a9d6bc977d2a7dd142c6
lahirutech <rmllbrathnayaka@gmail.com>
2018-09-19 17:46:18 +0530

    csrg token validation

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Material Design Bootstrap</title>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -17,10 +19,23 @@
     <link rel="stylesheet" href="{{asset('css/admin/style.css')}}" rel="stylesheet">
     <!-- custom js file -->
     <script type="text/javascript" src="{{asset('js/main.js')}}"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     @include('admin.adminnav')
     @yield('content')
     
 
 </body>
+
+
+</html>
+
