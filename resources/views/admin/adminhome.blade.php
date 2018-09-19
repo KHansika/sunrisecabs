35c9164af10b8b46f92516121d284b44f4ac4475
lahirutech <rmllbrathnayaka@gmail.com>
2018-09-19 17:42:32 +0530

    adding a container to Adminhome blade

--- resources/views/admin/adminhome.blade.php
+++ resources/views/admin/adminhome.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.admin')
 @section('content')
-        <div class="row">
-          @include('admin.pages.adminsidenav')
-  
+<div class="container-fluid">
+    @include('admin.pages.adminsidenav')
+</div>
+
+        <div class="row">  
           <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
               <h1 class="h2">Dashboard</h1>
@@ -15,7 +17,7 @@
               
             </div>
                             This is admin home
-                                </div>
+            </div>
                                     
                 
           
