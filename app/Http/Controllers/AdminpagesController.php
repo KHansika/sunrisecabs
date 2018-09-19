1900e62d5b11ddbced60acc0e1dacb99dbd16ca8
lahirutech <rmllbrathnayaka@gmail.com>
2018-09-19 17:41:12 +0530

    creating admin page Controller

--- app/Http/Controllers/AdminpagesController.php
+++ app/Http/Controllers/AdminpagesController.php
@@ -14,4 +14,7 @@ class AdminpagesController extends Controller
         $bookings= Booking::where('status',0)->get();
         return view('admin.pages.neworders')->with('bookings',$bookings);
     }
+    public function adminlogin(){
+        return redirect('/booking');
+    }
 }
