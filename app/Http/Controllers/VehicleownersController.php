ece7f0fdfddd26714242b28e6e2cec1870804435
lahirutech <rmllbrathnayaka@gmail.com>
2018-09-19 17:41:31 +0530

    creating the vehicle owner controller

--- app/Http/Controllers/VehicleownersController.php
+++ app/Http/Controllers/VehicleownersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Vehicleowner;
 
 class VehicleownersController extends Controller
 {
@@ -12,6 +13,19 @@ class VehicleownersController extends Controller
     }
     public function addvehicleowners(){
         return view('admin.pages.addowner');
+
+    }
+    public function tostore(Request $request){
+
+            $owner =new Vehicleowner;
+            $owner->voname=$request->input('voname');
+            $owner->nic=$request->input('nic');
+            $owner->address=$request->input('address');
+            $owner->contact=$request->input('contact');
+            $owner->email=$request->input('email');
+    
+            $owner->save();
+            return redirect('/addvehicleowners');
 
     }
 }
