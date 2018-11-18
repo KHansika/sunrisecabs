89b2d0fa382bcd7164e37cde408ca7c699e9370e
lahirutech <rmllbrathnayaka@gmail.com>
2018-11-18 11:40:41 +0530

    Creting Blade templates for vehicle owners profile

---
+++ resources/views/vowner/pages/vodashboard.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.admin')
+@section('content')
+<div class="container-fluid">
+    @include('vowner.pages.vownersidenav')
+</div>
+
+        <div class="row">  
+          <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+              <h1 class="h2">Dashboard</h1>
+              <div class="btn-toolbar mb-2 mb-md-0">
+                <div class="btn-group mr-2">
+                  
+                </div>
+                </button>
+              </div>
+              
+            </div>
+                            This is Vehicle Owner Profile
+            </div>
+                                    
+                
+          
+              <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+                <script src="{{asset('js/popper.min.js')}}"></script>
+                <!-- Icons -->
+                <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+                <script>
+                  feather.replace()
+                </script>
+
+
+          
