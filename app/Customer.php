5cb6cf6b62b40120c6e57de6de8ec646e0383ba7
lahirutech <rmllbrathnayaka@gmail.com>
2018-12-28 11:49:29 +0530

    creating the customer model

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    //one to one relatonship with Booking
+    public function booking(){
+        return $this->hasOne('App\Booking');
+    }
 }
