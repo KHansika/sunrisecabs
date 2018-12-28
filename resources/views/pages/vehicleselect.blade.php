65f2245590021f6e22b322b821c5ae13edf51b3d
lahirutech <rmllbrathnayaka@gmail.com>
2018-12-28 11:55:38 +0530

    Making the booking page items to a single form

--- resources/views/pages/vehicleselect.blade.php
+++ resources/views/pages/vehicleselect.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 {{-- Testing Form --}}
 {{-- date picker scdn --}}
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
 
 <body class="bg-light">
@@ -12,101 +13,84 @@
             {{-- image gallery --}}
             
             @include('inc.gallery')
+        {!!Form:: open(['action'=>['BookingsController@update',$booking->id], 'method'=>'POST','class'=>'needs-validation'])!!}
             <div class="row">
             <div class="col-md-4 order-md-2 mb-4">
+              
               <h4 class="d-flex justify-content-between align-items-center mb-3">
-                <span class="text-muted">Your cart</span>
-                <span class="badge badge-secondary badge-pill">3</span>
+                <span class="text-muted">Selected Vehicle</span>
               </h4>
-              <ul class="list-group mb-3">
-                <li class="list-group-item d-flex justify-content-between lh-condensed">
-                  <div>
-                    <h6 class="my-0">Product name</h6>
-                    <small class="text-muted">Brief description</small>
-                  </div>
-                  <span class="text-muted">$12</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between lh-condensed">
-                  <div>
-                    <h6 class="my-0">Second product</h6>
-                    <small class="text-muted">Brief description</small>
-                  </div>
-                  <span class="text-muted">$8</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between lh-condensed">
-                  <div>
-                    <h6 class="my-0">Third item</h6>
-                    <small class="text-muted">Brief description</small>
-                  </div>
-                  <span class="text-muted">$5</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between bg-light">
-                  <div class="text-success">
-                    <h6 class="my-0">Promo code</h6>
-                    <small>EXAMPLECODE</small>
-                  </div>
-                  <span class="text-success">-$5</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between">
-                  <span>Total (USD)</span>
-                  <strong>$20</strong>
-                </li>
-              </ul>
-    
-              <form class="card p-2">
-                <div class="input-group">
-                  <input type="text" class="form-control" placeholder="Promo code">
-                  <div class="input-group-append">
-                    <button type="submit" class="btn btn-secondary">Redeem</button>
-                  </div>
-                </div>
-              </form>
-            </div>
-            <div class="col-md-8 order-md-1">
-              {!!Form:: open(['action'=>['BookingsController@update',$booking->id], 'method'=>'POST','class'=>'needs-validation'])!!}
+
+            <ul class="list-group mb-3">
+                  <li class="list-group-item d-flex justify-content-between lh-condensed">
+                      <div id="cart">
+                        <h6 class="my-0" id="vname"></h6>
+                        {{-- <small class="text-muted">Brief description</small> --}}
+                      </div>
+                      {{-- <span class="text-muted">$12</span> --}}
+                  </li>
+                  <li>
+                      <img class="card-img-top vehicle-image vehicle"id="vimage" src="">
+                  </li>
+                  
+                  <li class="list-group-item d-flex justify-content-between lh-condensed">
+                    <div id="plateno">
+                        <h5 class="my-0" id="">Vehicle Features</h5>
+                      <h6 class="my-0" id="vcategory"></h6>
+                      {{-- <small class="text-muted">Brief description</small> --}}
+                    </div>
+                    {{-- <span class="text-muted">$5</span> --}}
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between">
+                    <span>Price (Per Day)</span>
+                    <strong id="vprice"></strong>
+                  </li>
+            </ul>
+          </div>
+              <div class="col-md-8 order-md-1">
+
               {{-- <form class="needs-validation" action=>['BookingsController@update',$Booking->id] novalidate> --}}
                 <div class="row">
-                  <div class="col-md-6 mb-3">
-                    <label for="pickup">Pickup Location (Mark On the Map )</label>
-                    <input type="text" name="piklocation" class="form-control" id="pickup" placeholder="" value="{{$booking->piklocation}}" required>
-                    <div class="invalid-feedback">
-                      Valid Pickup Location is required.
+                    <div class="col-md-6 mb-3">
+                      <label for="pickup">Pickup Location (Mark On the Map )</label>
+                      <input type="text" name="piklocation" class="form-control" id="pickup" placeholder="" value="{{$booking->piklocation}}" required>
+                      <div class="invalid-feedback">
+                        Valid Pickup Location is required.
+                      </div>
                     </div>
-                  </div>
-                  <div class="col-md-6 mb-3">
-                    <label for="dropdown">Drop Down Location (Mark On the Map)</label>
-                    <input type="text" name="droplocation" class="form-control" id="dropdown" placeholder="" value="" required>
-                    <div class="invalid-feedback">
-                      Valid Drop Down Loaction is required.
+                    <div class="col-md-6 mb-3">
+                      <label for="dropdown">Drop Down Location (Mark On the Map)</label>
+                      <input type="text" name="droplocation" class="form-control" id="dropdown" placeholder="" value="" required>
+                      <div class="invalid-feedback">
+                        Valid Drop Down Loaction is required.
+                      </div>
                     </div>
-                  </div>
                 </div>
                 <div class="row">
-                  <div class="col-md-4 mb-2">
-                    <label for="pickup">Pickup Date and time</label>
-                  <input type="text" name="pickdate" class="form-control" id="datepicker" placeholder="" value="{{$booking->date}}" required>
-                    {{-- Pickup time --}}
-                      
-                    <div class="invalid-feedback">
-                      Valid Pickup Date and time is required.
+                    <div class="col-md-4 mb-2">
+                      <label for="pickup">Pickup Date</label>
+                    <input type="date" name="pickdate" class="form-control" id="datepicker" placeholder="" value="{{$booking->date}}" required>
+                      {{-- Pickup time --}}
+                        
+                      <div class="invalid-feedback">
+                        Valid Pickup Date and time is required.
+                      </div>
+                    </div>
+                    <div class="col-md-4 mb-2">
+                      <label for="piktime">Pickup Time</label>
+                      <input type="time" name="piktime"class="form-control" id=" " placeholder="" value="" >
+                      <div class="invalid-feedback">
+                        Valid Drop Down Loaction is required.
+                      </div>
+                    </div>
+                    <div class="col-md-4 mb-2">
+                      <label for="lastName">Number Of Days</label>
+                      <input type="number" name="days"class="form-control" id="" min='1'  placeholder="No of Days" value="" required>
+                      <div class="invalid-feedback">
+                        Valid Drop Down Loaction is required.
+                      </div>
                     </div>
                   </div>
-                  <div class="col-md-4 mb-2">
-                    <label for="piktime">Pickup Time</label>
-                    <input type="time" name="piktime"class="form-control" id=" " placeholder="" value="" >
-                    <div class="invalid-feedback">
-                      Valid Drop Down Loaction is required.
-                    </div>
-                  </div>
-                  <div class="col-md-4 mb-2">
-                    <label for="lastName">Number Of Days</label>
-                    <input type="number" name="days"class="form-control" id="" min='1'  placeholder="No of Days" value="" required>
-                    <div class="invalid-feedback">
-                      Valid Drop Down Loaction is required.
-                    </div>
-                  </div>
-
-                </div>
                 <div class="row">
                   <div class="col-md-6 mb-3">
                     <label for="fname">First Name</label>
@@ -133,14 +117,11 @@
                     </div>
                   </div>
                 </div>
-
-                
-    
                 <div class="mb-3">
                   <label for="email">Email <span class="text-muted">(To send your invoice)</span></label>
                   <input type="email" name="email" class="form-control" id="email" placeholder="you@example.com">
                   <div class="invalid-feedback">
-                    Please enter a valid email address for shipping updates.
+                    Please enter a valid email address for Inovice and updates.
                   </div>
                 </div>
     
@@ -160,23 +141,26 @@
                   <input type="checkbox" class="custom-control-input" id="save-info">
                 </div>               
                 <button class="btn btn-primary btn-lg btn-block" type="submit">Place Your Booking</button>
-              {{Form::hidden('_method','PUT')}}
+
+                {{Form::hidden('_method','PUT')}}
               </form>
             </div>
           </div>
-</div>
-<script src="//code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+          
+    </div>
+{{-- <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script> --}}
   <script>
-  $(function() {
-    $( "#datepicker" ).datepicker();
-  });
-  $(function() {
-    $( "#datepicker1" ).datepicker();
-  });
-  $(function() {
-    $( "#datepicker2" ).datepicker();
-  });
+
+  // $(function() {
+  //   $( "#datepicker" ).datepicker();
+  // });
+  // $(function() {
+  //   $( "#datepicker1" ).datepicker();
+  // });
+  // $(function() {
+  //   $( "#datepicker2" ).datepicker();
+  // });
+  
   </script>
   
-@endsection 
