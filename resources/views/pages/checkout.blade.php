9dcc9214d5a1b0671617a2a5998ae69b52ba60af
lahirutech <rmllbrathnayaka@gmail.com>
2019-01-01 16:34:42 +0530

    Adjusting the id name in vehicle select form

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+
 {{-- Testing Form --}}
 {{-- date picker scdn --}}
 <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -7,48 +8,51 @@
 
         <div class="container">
           <div class="py-5 text-center">
-                    {{-- {!!Form:: open(['action'=>['BookingsController@update',$customer->id], 'method'=>'POST','class'=>'needs-validation'])!!} --}}
+            <form method="POST" class="needs-validation" action="/reservation" id="checkout-form">
+            {{-- {!!Form:: open(['action'=>'/reservations', 'method'=>'POST','class'=>'needs-validation'])!!} --}}
+            {{ csrf_field() }}
             <div class="row">
             <div class="col-md-4 order-md-2 mb-4">
               <h4 class="d-flex justify-content-between align-items-center mb-3">
                 <span class="text-muted">Purchased Vehicle</span>
               </h4>
 
-            <ul class="list-group mb-3">
+              <ul class="list-group mb-3" id="#cart">
                   <li class="list-group-item d-flex justify-content-between lh-condensed">
                       <div id="cart">
-                        <h6 class="my-0">Vehicle Image</h6>
+                        <h6 class="my-0" id="vname"> </h6>
                         {{-- <small class="text-muted">Brief description</small> --}}
                       </div>
                       {{-- <span class="text-muted">$12</span> --}}
                   </li>
-                  <li class="list-group-item d-flex justify-content-between lh-condensed">
-                    <div id='regno'>
-                      <h6 class="my-0">Price Perday</h6>
-                      <small class="text-muted">Brief description</small>
-                    </div>
-                  <span class="text-muted">{{$vehicle->price}}</span>
-                  <input name="perday" type="hidden" value="">       
-
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between lh-condensed">
-                    <div id="plateno">
-                      <h6 class="my-0">Descreption</h6>
-                      {{-- <small class="text-muted">Brief description</small> --}}
-                    </div>
-                    {{-- <span class="text-muted">$5</span> --}}
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between bg-light">                  
+                  <li class="list-group-item">
+                      <img class="card-img-top vehicle-image vehicle"id="vimage" src="/storage/cover_images/{{$vehicle->cover_image}}">
                   </li>
                   <li class="list-group-item d-flex justify-content-between">
-                    <span>Total (LKR)</span>
-                    <strong>$20</strong>
+                      <span>Vehicle Features</span>
+                    <strong id="vprice">{{$vehicle->category}}</strong>
+                    </li>
+                  <li class="list-group-item d-flex justify-content-between">
+                    <span>Price (Per Day)</span>
+                  <strong id="vprice">{{$vehicle->price}}</strong>
                   </li>
+                  <li class="list-group-item d-flex justify-content-between">
+                      <span>No Of days</span>
+                    <strong id="vprice">{{$booking->days}}</strong>
+                    </li>
+                  <li class="list-group-item d-flex justify-content-between">
+                      <span>Total Price</span>
+                  <strong id="vtprice">{{$booking->topay}}</strong>
+                    </li>
+                    <input type="text" name="bookingid"  value="{{$booking->id}}" hidden>
+                    <input type="text" name="vehicleid" value="{{$vehicle->id}}" hidden>
+
+
             </ul>
           </div>
               <div class="col-md-8 order-md-1">
 
-                    {!!Form:: open(['action'=>['BookingsController@update',$booking->id], 'method'=>'POST','class'=>'needs-validation'])!!}
+                    {{-- {!!Form:: open(['action'=>['addtocartsController@checkout'], 'method'=>'POST','class'=>'needs-validation','id'=>'checkout-form'])!!} --}}
                     <h4 class="d-flex justify-content-between align-items-center mb-3">
                             <span class="text-muted">Card Details</span>
                           </h4>                 
@@ -60,9 +64,13 @@
                             Valid First name is requires
                         </div>
                   </div>
+                {{-- <div id="charge-error" class="alert alert-danger {{!Session::has('error')?'hidden':''}}">
+                  {{Session::get('error')}}
+
+                </div> --}}
                   <div class="col-md-6 mb-3">
                     <label for="lName">Name On the Card</label>
-                    <input type="text" name="lname" class="form-control" id="dropdown" placeholder="Last Name" value="{{$customer->lname}}" required>
+                    <input type="text" id="cardname" name="lname" class="form-control" id="dropdown" placeholder="Last Name" value="{{$customer->lname}}" required>
                     <div class="invalid-feedback">
                       Valid last name is required.
                     </div>
@@ -71,28 +79,28 @@
                 <div class="row">
                         <div class="col-md-4 mb-3">
                           <label for="fname"> Card Number</label>
-                          <input type="number" name="cardnum" class="form-control" id="cardnum" placeholder="" value="" required>
+                          <input type="number" id="card-number"name="cardnum" class="form-control" id="cardnum" placeholder="" value="" required>
                           <div class="invalid-feedback">
                                   Valid First name is requires
                               </div>
                         </div>
                         <div class="col-md-3 mb-3">
                           <label for="lName">Expiery Year</label>
-                          <input type="number" name="year" class="form-control" id="year" placeholder="Year" value="" required>
+                          <input type="number" id="card-ex-year" name="year" class="form-control" id="year" placeholder="Year" value="" required>
                           <div class="invalid-feedback">
                             Valid last name is required.
                           </div>
                         </div>
                         <div class="col-md-3 mb-3">
                                 <label for="lName">Expiery Month</label>
-                                <input type="number" name="month" class="form-control" id="Month" placeholder="Month" value="" required>
+                                <input type="number" id="card-ex-month" name="month" class="form-control" id="Month" placeholder="Month" value="" required>
                                 <div class="invalid-feedback">
                                   Valid Month is required.
                                 </div>
                               </div>
                         <div class="col-md-2 mb-3">
                                 <label for="lName">CSV</label>
-                                <input type="number" name="lname" class="form-control" id="dropdown" placeholder="***" value="" required>
+                                <input type="number" id="card-cvs" name="lname" class="form-control" id="dropdown" placeholder="***" value="" required>
                                 <div class="invalid-feedback">
                                   Valid last name is required.
                                 </div>
@@ -121,12 +129,18 @@
                 <div class="custom-control custom-checkbox">
                   <input type="checkbox" class="custom-control-input" id="save-info">
                 </div>               
-                <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now</button>
+                <button class="btn btn-primary btn-lg btn-block" type="submit">Paying Now</button>
 
-                {{Form::hidden('_method','PUT')}}
+                {{-- {{Form::hidden('_method','PUT')}} --}}
               </form>
             </div>
           </div>
           
     </div>
+    @section('scripts')
+    <script type="text/javascript" src="https://js.stripe.com/v3/"></script>
+    {{-- <script type="text/javascript" src="{{asset('js/checkout.js')}}"></script> --}}
+
+
+    @endsection
   
