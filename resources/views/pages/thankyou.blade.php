1d0317b816dd274cc8ac58665f19df3e0065a2b4
lahirutech <rmllbrathnayaka@gmail.com>
2019-01-01 16:35:37 +0530

    Creting the Thank You page

---
+++ resources/views/pages/thankyou.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('content')
+<div class="jumbotron text-xs-center">
+        <h1 class="display-3">Thank You!</h1>
+        <p class="lead"><strong>Please check your email</strong> To get Your Invoice</p>
+        <hr>
+        <p>
+          Having trouble? <a href="">Contact us</a>
+        </p>
+        <p class="lead">
+          <a class="btn btn-primary btn-sm" href="https://bootstrapcreative.com/" role="button">Continue to homepage</a>
+        </p>
+      </div>
