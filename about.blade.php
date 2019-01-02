9b46ff4dfa7c6d04480f127a9dab5c8e220c19eb
KHansika <36556682+KHansika@users.noreply.github.com>
2019-01-02 12:10:38 +0530

    Add files via upload

---
+++ about.blade.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Sunrise Cabs</title>
+
+     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script type="text/javascript" src="{{asset('js/main.js')}}"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/mdb.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/business-frontpage.css')}}" rel="stylesheet">
+    <style>
+    .business-header {
+ background: url("../img/cover/aboutcover.jpg")no-repeat center center fixed;
+ height: 50vh;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+</style>
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark primary-color fixed-top">
+
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="#">Sunrise Cabs</a>
+
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    
+
+    <!-- Header with Background Image -->
+    <header class = "business-header">
+      
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row" style="margin-top:10px;">
+        <div class="col-sm-8">
+          <h2 class="mt-4"><b>About us</h2>
+          <p>Sunrise Cabs company Sri Lanka is a leading Car rental company established in the Kandy district which has maintained its leading position as a trustworthy, efficient and innovative one-stop-shop for all vehicle rental needs. We rent everything from standard sedans to top of the range luxury cars, vans and jeeps on self-drive or with driver at the cheapest rates in town.</b></p>
+          
+         
+        </div>
+       <div class="col-sm-4" style="background-color: #bac9e0">
+          <h2 class="mt-4"><b>Head Office</h2>
+          <address>
+            <strong>Sunrise Cabs</strong>
+            <br>No 5
+            <br>Dharmashoka Mawatha
+            <br>Kandy
+            <br>
+          </address>
+          <address>
+            <abbr title="Phone">Tel:</abbr>
+             077 781 0341
+            <br>
+            <abbr title="Email">Email:</abbr>
+            <a href="mailto:#">sunrisecabs1@gmail.com</a></b>
+          </address>
+        </div>
+      </div>
+
+      <div class = "row" style="margin-top:10px;">
+        <div class ="col-sm-12">
+        <img src="../img/cover/fleet.jpg" style="height:47vh;" alt="">
+      </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-4 my-4">
+
+
+		<div class="row">
+        <div class="col-sm-6 my-4">
+           <h4 class= "text-align:center"><b>Vision and Mission</b></h4>
+            <img src = "../img/cover/mission.jpg" height="200px" class="mx-auto d-block">
+            <p class= "text-center">To be the leading car rental service provider in Sri Lanka.</p>
+        </div>
+
+
+	<h3 style="text-align: center;">OUR SERVICE</h3>
+<p>&nbsp;</p>
+<p style="text-align: center;"> • Chose a vehicle as per your own convenience form a fully air-conditioned vehicle fleet<br /> of latest Luxury Cars, Super Luxury vans or 28-54 Seater Tourist Buses which can cater<br /> single person to large crew wishing to spend their Holidays in Sri Lanka.</p>
+<p style="text-align: center;">• Our drivers are decent, qualified and having over 20 years experience in the field<br /> roads conditions spend your holidays safely &amp;amp; comfortably.</p>
+<p style="text-align: center;">• Spend only for the kilometers you spent.</p>
+<p style="text-align: center;">• We can organize round trips as you wish. With our preview trips, you will find flexibility<br /> to stop at any place that you may interest.</p>
+<p style="text-align: center;">• With us, you will be able to spend your dream tour in historical and cultural locations,<br /> national parks with wild animals. And also we will bring you to botanical gardens,<br /> Panoramic Mountains and magnificent natural waterfalls.</p>
+<p style="text-align: center;">• We offer 24/7/365 roadside assistance island wide with a team ready for action. A pool<br /> of replacement vehicles is held at all times to ensure peace of mind. We are also<br /> backed up by a network of garages located all over the island and on the spot insurance<br /> assistance offering speedy attention.<br /> We wish to arrange accommodation packages such as Holiday villas and bungalows<br /> which are independently managed with excellent hospitality avoiding busy and noisy<br /> towns sold by average travel agents.</p>
+<p style="text-align: center;">SUNRISE Rent a Car makes it easy to compare car hire, without the legwork. We show<br /> you quote from top booking companies and car rental agencies all on one page so you<br /> can find the best offer at a glance. Narrow down the search results to find the perfect<br /> car rental package for your trip. Choose your preferred fuel policy, what features you<br /> need, what size of car you want, and more. What you see is what you get with<br />  
+              
+              <h4 class= "text-center"><b>Trust and Reliality</b></h4>
+                  <p class= "text-center">Make an online booking with a deposit and be certain that a vehicle of your choice will be assigned to you on the requested date and time. No excuses.</p>
+                </div>
+
+          <div class="col-sm-4 my-4">   
+              <h4 class= "text-center"><b>Convenience</b></h4>
+                When you pick up your vehicle, paperwork is kept to a minimum. Fill out the essentials and you are on your way. Hassle free to the max.</p>
+              </div>
+
+                <div class="col-sm-4 my-4">
+            <h4 class= "text-center"><b>Safety</b></h4>
+            <p>Choose from our fleet of latest model Japanese vehicles with the best safety features. All Shineway rental car Sri Lanka vehicles are equipped with driver and passenger airbags, seatbelts for all passengers and side collision bars on the doors and much more.</p>
+          </div>
+        </div>
+        <hr>
+
+
+       
+
+        <div class="col-sm-6 my-4">
+              <h4 class= "text-center"><b>Director</h4>
+                <img src = "../img/cover/team.jpg" height="100px" class="rounded-circle mx-auto d-block">
+                <br>
+              <p class= "text-center">Sasitha Palitha Kaluarachchi</b></p>
+              <p>The founder of the company, an old student of Trinity College, works as the Managing Director of the company</p>
+            </div>
+          </div>
+        
+          </div>
+   
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+    
+  </body>
+
+</html>
+ 
